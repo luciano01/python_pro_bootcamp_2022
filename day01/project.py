a532b651d64510397ada9205ca5656e6d7c5032a
Luciano <luciano01.dev@gmail.com>
2022-01-05 15:38:42 -0300

    Day 01

---
+++ day01/project.py
@@ -0,0 +1,10 @@
+# Band Name Generetor.
+print('Welcome to the Band Name Generetor.')
+
+city = input('What\'s name of the city you grew up in? ')
+print(city)
+
+pet_name = input('What\'s your pet\'s name? ')
+print(pet_name)
+
+print('Your band name could be ' + city + ' ' + pet_name)
